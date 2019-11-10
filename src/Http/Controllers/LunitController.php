f30e8f1b7d06dd6f8d55e113bcee2ad3ea7da58f
lixiyongtry <1204155766@qq.com>
2019-11-11 06:29:48 +0800

    基于laravel的测试组件

---
+++ src/Http/Controllers/LunitController.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2019/11/10 0010
+ * Time: 7:47
+ */
+
+namespace Lixiyong\LunitLaravel\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Routing\Controller;
+
+/**
+ * use Illuminate\Routing\Controller 这是laravel的基类控制器
+ * index() 访问测试的页面
+ * store() 是表示用来执行需要测试的方法
+ */
+
+class LunitController extends Controller
+{
+    public function index()
+    {
+        return view('lunit::index');
+    }
+
+    public function store(Request $request)
+    {
+        $namespace = $request->input('namespace');
+        $className = $request->input('className');
+        $action    = $request->input('action');
+        $param     = $request->input('param');
+        $class = ($className == "") ? $namespace : $namespace.'\\'.$className;
+        //要替换的值 需要的结果
+        $class = str_replace("/","\\",$class);
+        $object = new $class();
+        $param = ($param=="") ? [] : explode('|',$param);
+        $data = call_user_func_array([$object,$action],$param);
+        return (is_array($data)) ? json_encode($data) : dd($data);
+    }
+}
